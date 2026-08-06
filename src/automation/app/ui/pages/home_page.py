3e6edad4f2eadaacc266458a36e2a7931c4135e6
barakshamir <barak20000@gmail.com>
2026-08-06 18:54:21 +0300

    f

--- src/automation/app/ui/pages/home_page.py
+++ src/automation/app/ui/pages/home_page.py
@@ -9,15 +9,3 @@ class HomePage(BasePage):
     @property
     def search_component(self) -> SearchComponent:
         return SearchComponent(self.page)
-
-    # @property
-    # def search_input(self) -> Locator:
-    #     return self.page.locator("#twotabsearchtextbox")
-
-    # @property
-    # def search_button(self) -> Locator:
-    #     return self.page.locator("#nav-search-submit-button")
-
-    # def search_item(self, text: str) -> None:
-    #     self.fill(self.search_input, text)
-    #     self.click(self.search_button)
