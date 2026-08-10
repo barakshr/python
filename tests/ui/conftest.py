a576757280e93c613c2240ab3752ec700e13140e
barakshamir <barak20000@gmail.com>
2026-08-10 20:07:35 +0300

    f

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -2,8 +2,8 @@ import pytest
 from playwright.sync_api import Page
 
 from automation.app.ui import LoginPage
-from automation.core.config import Settings
-from automation.app.ui import HomePage
+
+pytest_plugins = ["automation.core.ui.pytest_plugin"]
 
 
 @pytest.fixture(scope="function")
@@ -11,8 +11,3 @@ def login_page(page: Page):
     print("before_test")
     yield LoginPage(page)
     print("after_test")
-
-
-# @pytest.fixture
-# def login_page(page: Page, settings: Settings, before_after_test: None):
-#     return LoginPage(page)
